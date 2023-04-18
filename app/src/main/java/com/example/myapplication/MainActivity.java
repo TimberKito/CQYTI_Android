85ed45f07e7a99a8db7aa6e401a1dd4c54b36d45
TimberKito <timberkitowang@gmail.com>
2023-04-18 17:17:51 +0800

    APP首页布局实现

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -54,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
                         if (userSetPass.equals(userPass)) {
                             // 登陆成功
                             Toast.makeText(MainActivity.this, "登录成功！", Toast.LENGTH_SHORT).show(); // 消息展示结果(位置，内容，时间)
+                            // 跳转页面
+                            Intent it = new Intent(MainActivity.this,IndexActivity.class);
+                            startActivity(it);
                         } else {
                             // 密码错误
                             Toast.makeText(MainActivity.this, "密码错误！", Toast.LENGTH_SHORT).show();
